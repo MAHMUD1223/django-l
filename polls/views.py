cb4ee06753d827dfcd8da4ff042c4462ea7b9b62
MAHMUD1223 <117104429+MAHMUD1223@users.noreply.github.com>
2023-11-30 13:58:41 +0000

    making some modifications till templates

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(req):
-    return HttpResponse("hi")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(req, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(req, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(req, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
